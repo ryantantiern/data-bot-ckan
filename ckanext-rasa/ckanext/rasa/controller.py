8f7d4c6e8da226d2564c28fbb3b01fc783b2f7fa
Ryan <ryantantiern@gmail.com>
2018-02-28 17:41:44 +0000

    complete chat interface

--- ckanext-rasa/ckanext/rasa/controller.py
+++ ckanext-rasa/ckanext/rasa/controller.py
@@ -1,8 +1,41 @@
 import ckan.plugins as p
+import json
+import time
 from ckan.lib.base import BaseController
-from ckan.common import request, response
+from ckan.common import request, response, session
+
+def get_response_from_rasa(text):
+    # Need to handle sender_id
+    if not session.get("sender_id"):
+        session["sender_id"] = session.id
+        session.save()
+
+    # RASA Goes here
+    response = "this is an automated response"
+    return response
+
 
 class RasaPluginController(BaseController):
     def send_user_message(self):
-        response.body = "all ok!"
+        body = {}
+        try:
+            request_body = json.loads(request.body)
+        except Exception as e:
+            # Didn't get appropriate JSON format
+            response.status = 404
+            return
+
+        # Didn't get JSON object or body doesn't have text field
+        if not isinstance(request_body, dict) or not request_body.get("text"):
+            response.status = 404
+            return
+
+        bot_response = get_response_from_rasa(request_body["text"])
+
+        if not bot_response:
+            body["error"] = True
+            bot_response = "Something went wrong. Terminating and restarting..."
+
+        body["bot"] = bot_response
+        response.body = json.dumps(body)
         return
--- ckanext-rasa/ckanext/rasa/fanstatic/master.js
+++ ckanext-rasa/ckanext/rasa/fanstatic/master.js
@@ -1,23 +1,69 @@
 var chatbox = $(".chatbox")
+var chat_start = $("#chat-start")
 $("#user-input").val('')
-chatbox.scrollTop = chatbox.scrollHeight - chatbox.clientHeight
+chatbox.animate({scrollTop: chat_start.height() }, "slow")
+BASE_URL = document.location.href
 
 function submit_query(form){
   /*Entry from DOM*/
-  if (form.elements["user-input"].value){
-    // Make AJAX request. Check that not wewaiting for AJAX request, otherwise void input
-    _append(form.elements["user-input"].value, true)
+  route = "rasa/user/message"
+  url = BASE_URL + route
+  text = form.elements["user-input"].value
+
+  if (text){
+    _append(text, true)
+
+    chatbox.animate({scrollTop: chat_start.height() }, "slow")
+
+    $(document).ajaxStart(function(){
+    }).ajaxStop(function(){
+    })
+
     $("#user-input").val('')
+    // Make AJAX request. Check that not wewaiting for AJAX request, otherwise void input
+    response = send_user_message(url, "POST", text).then(
+      bot_response,
+      handle_request_error
+    )
   }
 }
-
 
 function _append(text, is_human){
   message = format_incoming_message(text, is_human)
   li = construct_li(message)
-  console.log(li)
   $("#chat-start").append(li)
 }
+
+// Bot responses
+
+function handle_request_error(response){
+  _append("ERROR 404: something went seriously wrong", false)
+}
+
+
+function bot_response(response){
+  if (response["error"]){
+    // Handle error
+    return
+  }
+  _append(response["bot"], false)
+}
+
+// AJAX using promises
+
+function send_user_message(url, methodType, text){
+  data = {
+    "text" : text
+  }
+  return $.ajax({
+    url: url,
+    method: methodType,
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    dataType: "json"
+  })
+}
+
 
 // Format incoming messages
 
